6b88916db0188dbd107b19b024b5edcf4b772c98
Dearhuan <clearhuan@qq.com>
2022-09-04 05:29:55 +0000

    Deploying to gh-pages from  @ 178a42ca4aa03aad2ee34bbe058f3f1a8cc07c89 🚀

---
+++ assets/ts-study-interface.html.655fe047.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-71f6a4ab","path":"/notes/ts-study-interface.html","title":"TypeScript-\u63A5\u53E3","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1662269316000,"contributors":[{"name":"Hello","email":"clearhuan@qq.com","commits":1}]},"filePathRelative":"notes/ts-study-interface.md"}');export{t as data};
